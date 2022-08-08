dd6c5c1036e4a3f1d0123c6261fe5db7e5694381
CoreyLongDev <CoreyLong90@icloud.com>
2022-08-08 08:47:13 -0400

    working on CSS for header / navi component

--- src/Components/HeaderNavi/HeaderNavi.js
+++ src/Components/HeaderNavi/HeaderNavi.js
@@ -4,19 +4,21 @@ import './HeaderNavi.css'
 import Logo from '../HeaderNavi/logoWhite.png'
 
 const HeaderNavi = () => {
-    return(
-    <>
-        <img id='logo' src={Logo} alt=''/>
+    return (
+        <>
+            <div id="header">
+                <img id='logo' src={Logo} alt='' />
 
-        <Navbar>
-            <Container className="navigation">
-                <Nav.Link href="./Work">Work</Nav.Link>
-                <Nav.Link href="./About">About</Nav.Link>
-                <Nav.Link href="./Contact">Contact</Nav.Link>
-            </Container>
-        </Navbar>
-        
-    </>
+                <Navbar>
+                    <Container className="navigation">
+                        <Nav.Link href="./Work">Work</Nav.Link>
+                        <Nav.Link href="./About">About</Nav.Link>
+                        <Nav.Link href="./Contact">Contact</Nav.Link>
+                    </Container>
+                </Navbar>
+                <div className="clear"></div>
+            </div>
+        </>
     )
 }
 
