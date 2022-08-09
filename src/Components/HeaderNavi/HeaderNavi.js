a9b2b4f407f74879fb2ef262e281b6dd8e8f9283
CoreyLongDev <CoreyLong90@icloud.com>
2022-08-09 15:41:12 -0400

    big changes. styling and photos added in homeJS. headernavi working and finished for now. changes to routes, work now split to a game and web page to show different materials.

--- src/Components/HeaderNavi/HeaderNavi.js
+++ src/Components/HeaderNavi/HeaderNavi.js
@@ -7,13 +7,15 @@ const HeaderNavi = () => {
     return (
         <>
             <div id="header">
+                <Nav.Link href="./">
                 <img id='logo' src={Logo} alt='Personel brand logo for software developer Corey Long.' />
-
+                </Nav.Link>
                 <Navbar>
                     <Container className="navigation">
-                        <Nav.Link href="./Work">Work</Nav.Link>
-                        <Nav.Link href="./About">About</Nav.Link>
-                        <Nav.Link href="./Contact">Contact</Nav.Link>
+                        <Nav.Link href="./web">Web</Nav.Link>
+                        <Nav.Link href="./games">Games</Nav.Link>
+                        <Nav.Link href="./about">About</Nav.Link>
+                        <Nav.Link href="./contact">Contact</Nav.Link>
                     </Container>
                 </Navbar>
                 <div className="clear"></div>
--- src/Pages/About.js
+++ src/Pages/About.js
@@ -2,7 +2,14 @@ import React from "react";
 
 const About = () => {
     return(
-    <h1>About Page Working</h1>
+    <>
+    <h3>testing</h3>
+    <p>what it do baby boo</p>
+    <br />
+    <p>ya boy aint a jew</p>
+    <br />
+    <p>wanna watch me take a poo</p>
+    </>
     )
 }
 
