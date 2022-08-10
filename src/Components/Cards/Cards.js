29ea16c815d7b6d2893e1bcc6f28c15a3976222e
CoreyLongDev <CoreyLong90@icloud.com>
2022-08-10 08:56:16 -0400

    working on cards and browsers are feeling froggy. savings work to restart machine

--- src/Components/Cards/Cards.js
+++ src/Components/Cards/Cards.js
@@ -4,21 +4,23 @@ import { Card, Image } from "react-bootstrap";
 
 function Cards(props) {
     return (
-        <>
-            <Card style={{ width: '18rem' }}>
-                <Card.Img src={props.imgURL} />
+        <div className="cardHolder">
+            <Card className="card">
+                <Card.Img 
+                    src={props.imgURL}
+                    // style={{ maxWidth: "550px", maxHeight: "auto" }}
+                />
                 <Card.Body>
-                <Card.Title>{props.name}</Card.Title>
-                <Card.Title>{props.subTitle}</Card.Title>
-                <Card.Title>{props.info}</Card.Title>
+                    <Card.Title>{props.name}</Card.Title>
+                    <Card.Title>{props.subTitle}</Card.Title>
+                    <Card.Title>{props.info}</Card.Title>
                     <Card.Text>
-                        Some quick example text to build on the card title and make up the
-                        bulk of the card's content.
+                        pending paragraph
                     </Card.Text>
                 </Card.Body>
             </Card>
 
-        </>
+        </div>
     )
 }
 
--- src/Components/Cards/webData.js
+++ src/Components/Cards/webData.js
@@ -12,16 +12,16 @@ const webData = [
         name: "Trail Blazers",
         subTitle: "MongoDB - Express - React - Node",
         imgURL: "https://i.ibb.co/0t5BBLB/snapshot-trail-1.png",
-        info: "blah blah blah DOM DOM DOOOOM",
+        info: "Multi-Contributor Project",
         info2: "secondary info paragraph form",
         githubLink: "http://www.github.com",
         liveSite: "http://www.deployment.com"
     },
     {
         name: "Giphy Searcher",
-        subTitle: "Front-End React Project with 3rd Party API Call",
+        subTitle: "Front-End React Project",
         imgURL: "https://i.ibb.co/MkgZ5rj/Screen-Shot-2022-08-10-at-7-52-01-AM.png",
-        info: "blah blah blah DOM DOM DOOOOM",
+        info: "Search application resulting from Giphy API",
         info2: "secondary info paragraph form",
         githubLink: "http://www.github.com",
         liveSite: "http://www.deployment.com"
