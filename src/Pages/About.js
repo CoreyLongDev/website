3b0a4af98705d2465cbcb464f6a570d70e3ebbbe
CoreyLongDev <CoreyLong90@icloud.com>
2022-08-11 12:38:04 -0400

    working on limited footer for about and contact page. also wrote out quick bio on about page

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -6,9 +6,15 @@ const About = () => {
     return(
     <>
     <img src={LockedDoor} id="officePeek" />
-    <h3>Here We Go!</h3>
+    <h3>Welcome to My World</h3>
     <div className="aboutParagraph">
-        <p>blah blah blah</p>
+        <p>Hi my name is Corey,
+            <br />
+        I am a Software Developer from the Carolinas. I Recently graduated from General Assembly's Software Engineering Immersive Program and I'm currently obtaining my Bachelors of Science in Computer Science from Western Governors University.
+        I grew up playing games and drawing a lot, so naturally tech always interested me as a means of expression. When i was 20 i joined the United States Air Force under the job title Structural Engineering Technician because at the time i thought i would be happiest as a Environemnt Artist in the Games end of the industry.
+        little did i know life would get in the way of my college attempts for a good while. after spending most of my 20s in a lucrative and accomplished career in Construction Logistics i decided enough was enough and started getting back into tech.
+        While i was still drawing i started to delve into actual development and programming and found the challenge and extensive research behind every project inspired me to try harder at every turn. So since that fateful day ive been here on the web slinging away at projects to be the best i can. 
+        </p>
     </div>
     </>
     )
--- src/Pages/Contact.js
+++ src/Pages/Contact.js
@@ -9,16 +9,18 @@ const Contact = () => {
             <Container className="contactContainer">
                 <Form>
                     <Form.Group className="mb-3" controlId="email">
-                        <Form.Label>Email address</Form.Label>
+                        <Form.Label>Email</Form.Label>
                         <Form.Control type="email" placeholder="name@example.com" required />
                     </Form.Group>
                     <InputGroup>
-                        <InputGroup.Text>Talk to Me</InputGroup.Text>
+                        <InputGroup.Text>Speak</InputGroup.Text>
                         <Form.Control as="textarea" aria-label="With textarea" required/>
                     </InputGroup>
                     <Button variant="outline-info" size="lg" value="Submit">Send</Button>
                 </Form>
             </Container>
+
+
         </>
     )
 }
