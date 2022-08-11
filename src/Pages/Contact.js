eb5a44137a4ec6935e41a34c554f437ed6e328f2
CoreyLongDev <CoreyLong90@icloud.com>
2022-08-11 12:16:34 -0400

    basic contact form completed

--- src/Pages/Contact.js
+++ src/Pages/Contact.js
@@ -1,11 +1,24 @@
 import React from "react";
 import '../App.css'
-import { Form, Container } from "react-bootstrap";
+import { Form, Container, InputGroup, Button } from "react-bootstrap";
 
 const Contact = () => {
-    return(
+    return (
         <>
             <p id="topMessage">Let's get <strong>Connected</strong>!</p>
+            <Container className="contactContainer">
+                <Form>
+                    <Form.Group className="mb-3" controlId="email">
+                        <Form.Label>Email address</Form.Label>
+                        <Form.Control type="email" placeholder="name@example.com" required />
+                    </Form.Group>
+                    <InputGroup>
+                        <InputGroup.Text>Talk to Me</InputGroup.Text>
+                        <Form.Control as="textarea" aria-label="With textarea" required/>
+                    </InputGroup>
+                    <Button variant="outline-info" size="lg" value="Submit">Send</Button>
+                </Form>
+            </Container>
         </>
     )
 }
